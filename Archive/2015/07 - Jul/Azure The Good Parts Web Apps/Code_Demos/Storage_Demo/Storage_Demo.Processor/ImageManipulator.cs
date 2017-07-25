d9a976c185fb209a7c7cdeef5b8e43dada527162
richard <rtasker@vqcomms.com>
2017-07-25 19:07:46 +0100

    Restructuring Folders

---
+++ Archive/2015/07 - Jul/Azure The Good Parts Web Apps/Code_Demos/Storage_Demo/Storage_Demo.Processor/ImageManipulator.cs
@@ -0,0 +1,26 @@
+﻿namespace Storage_Demo.Processor
+{
+    using System.IO;
+    using ImageResizer;
+
+    public class ImageManipulator
+    {
+        public byte[] ResizeImage(byte[] image, int width)
+        {
+            return ProcessImage(image, new Instructions { Width = width });
+        }
+
+        private static byte[] ProcessImage(byte[] image, Instructions instructions)
+        {
+            using (var outputImage = new MemoryStream())
+            {
+                using (var inputImage = new MemoryStream(image))
+                {
+                    var job = new ImageJob(inputImage, outputImage, instructions);
+                    job.Build();
+                    return outputImage.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ Archive/2015/07 - Jul/Azure The Good Parts Web Apps/Code_Demos/Storage_Demo/Storage_Demo.Shared/Storage/AzureQueueService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using Storage_Demo.Shared.Messages;
+
+namespace Storage_Demo.Shared.Storage
+{
+    public class AzureQueueService
+    {
+        private readonly CloudQueue _queue;
+
+        public AzureQueueService(CloudQueueClient queueClient, string queueName)
+        {
+            _queue = queueClient.GetQueueReference(queueName);
+            _queue.CreateIfNotExists();
+        }
+
+        public void AddMessage(ResizeImageMessage message)
+        {
+            var serializeMessage = JsonConvert.SerializeObject(message);
+            _queue.AddMessage(new CloudQueueMessage(serializeMessage));
+        }
+
+        public CloudQueueMessage GetMessage()
+        {
+            return _queue.GetMessage();
+        }
+
+        public void DeleteMessage(CloudQueueMessage message)
+        {
+            _queue.DeleteMessage(message);
+        }
+    }
+}
